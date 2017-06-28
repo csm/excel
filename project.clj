52513e2afaa0a5ee216313e488ac4b16f911157e
Casey Marshall <casey.s.marshall@gmail.com>
2017-06-28 14:42:12 -0700

    Bump version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.bfpcorporation/excel "0.2.4"
+(defproject io.bfpcorporation/excel "0.2.5-SNAPSHOT"
   :description "A thin Clojure wrapper around a small part of Apache POI for
                 reading .xlsx files."
   :url "http://github.com/csm/excel"
